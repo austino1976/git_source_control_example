942a81e232779508b0a8b9ac0ee54b51106f2c34
Austin <austino@xilinx.com>
2022-02-14 17:07:56 +0000

    host.cpp edit 1402

--- logs/builder.py
+++
@@ -1,8 +0,0 @@
-# 2022-02-08T16:06:31.483687
-import vitisng
-
-client = vitisng.create_client()
-client.set_workspace(path="/proj/xirsqa/impflow/austino/2022.1/workspaces/git_source_control_example")
-
-app = client.create_app(name="concurrent_kernel_execution", platform="/proj/xbuilds/2022.1_daily_latest/internal_platforms/xilinx_aws-vu9p-f1_shell-v04261818_201920_3/xilinx_aws-vu9p-f1_shell-v04261818_201920_3.xpfm", template="vitis_examples/host/concurrent_kernel_execution")
-
---
+++ logs/builder_100156.py
@@ -0,0 +1,8 @@
+# 2022-02-14T16:52:22.631076
+import vitisng
+
+client = vitisng.create_client()
+client.set_workspace(path="/proj/xirsqa/impflow/austino/2022.1/workspaces/git_source_control_example")
+
+vitisng.dispose()
+
